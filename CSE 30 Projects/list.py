b58a17adcd8129e98dac511b2553c1c757fcb49f
rjmallik <57155009+rjmallik@users.noreply.github.com>
2024-03-25 19:53:59 -0700

    Delete CSE 30 Projects directory

--- CSE 30 Projects/list.py
+++
@@ -1,2 +0,0 @@
-def list_comprehension(listA):
-    return [listA[x] + listA[x+1] for x in range(len(listA)-1) if len(listA)>1]
--- CSE 30 Projects/practice.py
+++
@@ -1,3 +0,0 @@
-
-f = [ 0 if x % 3 == 0 else 1 for x in range (1,11) ]
-print(f)
